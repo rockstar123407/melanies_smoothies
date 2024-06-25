d9174d25564c49c64125d313c03b662e0717b71e
rockstar123407 <rohit.pradhan@barclays.com>
2024-06-25 17:08:15 +0530

    Update streamlit_app.py
    add datastream

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,4 +48,5 @@ if ingredients_list:
 # New section to display frutyvise information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
+fv_dt = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
