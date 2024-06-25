ae6c24befb0c5f18a9cd9e1e931d8a6ad3f4f15e
rockstar123407 <rohit.pradhan@barclays.com>
2024-06-25 18:33:11 +0530

    Update streamlit_app.py
    allignment

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,13 +32,13 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
-        fv_dt = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
-    #st.write(ingredients_string)
+            ingredients_string += fruit_chosen + ' '
+            st.subheader(fruit_chosen + ' Nutrition Information')
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+            fv_dt = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+            #st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
     st.write(my_insert_stmt)
